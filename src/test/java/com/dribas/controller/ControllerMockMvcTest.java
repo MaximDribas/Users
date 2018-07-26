72f27d0241db1cfc7531e2d754589b708fd4df59
Maxim Dribas <mvdribas@gmail.com>
2018-07-26 11:15:46 +0300

    Added ControllerIntegrationTest.

---
+++ src/test/java/com/dribas/controller/ControllerMockMvcTest.java
@@ -0,0 +1,29 @@
+package com.dribas.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerMockMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testMainPageReturnsMethodGetEmptyUsersArray () throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("[]")));
+    }
+}
